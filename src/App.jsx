5dec38fcbb0b0ce178d41eda54af4e77c94276a4
Campospdp <campospedro187@gmail.com>
2026-02-04 10:33:13 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/theme.css'
+import './styles/global.css'
+export function App() {
+
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <p>Vai Corinthians</p>
+      <input />
+      <button className="square">✅</button>
+    </>
+  )
+}
+
+//export default App
