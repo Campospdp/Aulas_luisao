4f7636be6f65d7e3cb22b057f44597f7988b34ea
Campospdp <campospedro187@gmail.com>
2026-02-11 09:13:31 -0300

    concluído

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './styles/theme.css'
 import './styles/global.css'
 export function App() {
@@ -14,24 +13,3 @@ export function App() {
 }
 
 //export default App
-=======
-// src/App.jsx
-import './theme.css';
-import './global.css';
-
-function App() {
-  return (
-    <main>
-      <h1>Acesso ao Sistema</h1>
-      <p>Insira os seus dados para continuar.</p>
-      
-      <input type="email" placeholder="E-mail" />
-      <input type="password" placeholder="Palavra-passe" />
-      
-      <button>Entrar</button>
-    </main>
-  );
-}
-
-export default App;
->>>>>>> f1ca418 (login dark mode concluído)
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import { App } from './App.jsx'
 //import App from './App.jsx'
-=======
-import './index.css'
-import App from './App.jsx'
->>>>>>> f1ca418 (login dark mode concluído)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
