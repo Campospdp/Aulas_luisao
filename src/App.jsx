4c50fb7c1066a0162a23f44b7d30753743f8c7ed
Campospdp <campospedro187@gmail.com>
2026-02-11 09:06:33 -0300

    login dark mode concluído

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './styles/theme.css'
 import './styles/global.css'
 export function App() {
@@ -13,3 +14,24 @@ export function App() {
 }
 
 //export default App
+=======
+// src/App.jsx
+import './theme.css';
+import './global.css';
+
+function App() {
+  return (
+    <main>
+      <h1>Acesso ao Sistema</h1>
+      <p>Insira os seus dados para continuar.</p>
+      
+      <input type="email" placeholder="E-mail" />
+      <input type="password" placeholder="Palavra-passe" />
+      
+      <button>Entrar</button>
+    </main>
+  );
+}
+
+export default App;
+>>>>>>> f1ca418 (login dark mode concluído)
